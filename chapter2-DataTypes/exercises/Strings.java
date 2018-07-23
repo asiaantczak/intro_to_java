f80bac970477cdc7e72e3e769e475e2e21c3c6b1
Asia Antczak <aasiaantczak@gmail.com>
2018-07-23 15:10:27 +0100

    Strings exercise

--- chapter2-DataTypes/exercises/Strings.java
+++ chapter2-DataTypes/exercises/Strings.java
@@ -1,12 +1,12 @@
 public class Strings {
   public static void main(String[] args) {
     // declare the first name
-
+    String a="Asia";
     // declare the second name
-
+    String b="Bernie";
     // create your message
-
+    String message=a + " and " + b  + " are learning Java!";
     // print your message!
-
+    System.out.println(message);
   }
 }
