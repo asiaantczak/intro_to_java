4a43566d8c23b91175134149567cf32342f0dc45
Asia Antczak <aasiaantczak@gmail.com>
2018-07-26 15:39:55 +0100

    Add loop through array

--- chapter3-JavaBuzz/exercises/LoopThroughArray.java
+++ chapter3-JavaBuzz/exercises/LoopThroughArray.java
@@ -1,3 +1,10 @@
 // can you remember how to set up your main function?
-
-// loop through an integer array and print out each element
+public class LoopThroughArray {
+  public static void main(String[] args) {
+    // loop through an integer array and print out each element
+    int[] nums = { 1, 2, 3, 4, 5 };
+    for (int i = 0; i < nums.length; i++) {
+      System.out.println(nums[i]);
+    }
+  }
+}
