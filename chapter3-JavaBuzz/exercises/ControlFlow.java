579861fefa79352fdda233e887757b44943c106f
Asia Antczak <aasiaantczak@gmail.com>
2018-07-26 17:32:07 +0100

    JavaBuzz exercise

--- chapter3-JavaBuzz/exercises/ControlFlow.java
+++ chapter3-JavaBuzz/exercises/ControlFlow.java
@@ -23,7 +23,5 @@ public class ControlFlow {
         break;
       }
     }
-
-
   }
 }
--- chapter3-JavaBuzz/exercises/JavaBuzz.java
+++ chapter3-JavaBuzz/exercises/JavaBuzz.java
@@ -1,5 +1,17 @@
 // can you remember how to set up your main function?
-
-// "Java" if divisible by 3
-// "Buzz" if divisible by 5
-// "JavaBuzz" if divisible by 15
+public class JavaBuzz {
+  public static void main(String[] args) {
+    for(int i = 1; i <= 100; i++ ) {
+      if (i % 15 == 0) {
+        // "JavaBuzz" if divisible by 15
+        System.out.println("JavaBuzz");
+      } else if (i % 5 == 0) {
+        // "Buzz" if divisible by 5
+        System.out.println("Buzz");
+      } else {
+        // "Java" if divisible by 3
+        System.out.println("Java");
+      }
+    }
+  }
+}
