3ada37a96e8a106eb7b52f5d362da641036bbcb6
Asia Antczak <aasiaantczak@gmail.com>
2018-08-09 18:16:31 +0100

    Test refactoring

--- chapter4-JavaBuzz-TDD-style/exercises/JavaBuzzTest.java
+++ chapter4-JavaBuzz-TDD-style/exercises/JavaBuzzTest.java
@@ -3,27 +3,25 @@ import org.junit.Test;
 
 public class JavaBuzzTest {
 
+  JavaBuzz javabuzz = new JavaBuzz();
+
   @Test
   public void returnsJavaIfDivisibleByThree() {
-    JavaBuzz javabuzz = new JavaBuzz();
     assertEquals(javabuzz.convert(3), "Java");
   }
 
   @Test
   public void returnBuzzIfDivisibleByFive() {
-    JavaBuzz javabuzz = new JavaBuzz();
     assertEquals(javabuzz.convert(5), "Buzz");
   }
 
   @Test
   public void returnJavaBuzzIfDivisibleByFiveAndThree() {
-    JavaBuzz javabuzz = new JavaBuzz();
     assertEquals(javabuzz.convert(15), "JavaBuzz");
   }
 
   @Test
   public void returnNumIfNotDivisibleByThreeFiveOrFifteen() {
-    JavaBuzz javabuzz = new JavaBuzz();
     assertEquals(javabuzz.convert(2), "2");
   }
 }
